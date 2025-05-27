2e71061b432c93b17e23e271b8bc04b208373875
Sean <121770894+itsbonface@users.noreply.github.com>
2025-05-27 23:15:20 +0300

    Update responses.py

--- responses.py
+++ responses.py
@@ -2,13 +2,13 @@ import random
 
 # all responses can also be stored here
 about_me = "Well, I'm a simple chatbot for now. But who knows?\n\
-     I might get complex and take over your world with time.\n\
+     My algorithm might get complex with time.\n\
      Maybe I'm just kidding, maybe not."
 
 def unknown_message():
     response = [
         'Could you please re-phrase that?',
-        'What does that even mean, nigga?',
+        'What does that even mean?',
         'That sound great with me.',
         'Okay, I\'m lost'
     ][random.randrange(4)]
